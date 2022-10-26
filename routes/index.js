287b5897f107bf4cc47ecac0763c499ee0307439
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-10-26 16:46:19 +0700

    [ADD] Auth Middleware

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const routes = require('express').Router()
 
+const authMiddleware = require('../middlewares/auth.middleware')
+
 routes.use('/users', require('./users.route'))
 routes.use('/auth', require('./auth.route'))
-routes.use('/profile', require('./profile.route'))
+routes.use('/profile', authMiddleware, require('./profile.route'))
 
 module.exports = routes
