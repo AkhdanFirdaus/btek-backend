6d823959ba9209a7b2536a9ad97bdc4a1926ec3e
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-10-26 16:39:04 +0700

    [ADD] Profile Controller

---
+++ controllers/profile.controller.js
@@ -0,0 +1,46 @@
+const profileModel = require('../models/profile.model')
+
+exports.readProfileById = async (req, res) => {
+  try {
+    const id = req.params.id || req.userData.id
+    const profile = await profileModel.selectProfileByUserId(id)
+    if (profile.rowCount) {
+      return res.json({
+        success: true,
+        message: 'Profile user with id ' + id,
+        result: profile.rows[0]
+      })
+    }
+    return res.status(400).json({
+      success: false,
+      message: 'User with id ' + id + ' not found'
+    })
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: 'Error : ' + err.message
+    })
+  }
+}
+
+exports.updateProfile = async (req, res) => {
+  try {
+    const profile = await profileModel.updateProfileByUserId(req.userData.id, req.body)
+    if (profile.rowCount) {
+      return res.json({
+        success: true,
+        message: 'Update profile success',
+        result: profile.rows[0]
+      })
+    }
+    return res.status(400).json({
+      success: false,
+      message: 'Failed update user with id ' + req.userData.id + ', not found'
+    })
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: 'Error : ' + err.message
+    })
+  }
+}
